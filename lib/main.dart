8f6cec6e55ed918c2412c37dbd317c6d34f66f0d
Bojan227 <blazevski.bojan1@gmail.com>
2023-10-04 11:30:01 +0200

    add: theme config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:crypto_app/core/config/app_config.dart';
 import 'package:crypto_app/core/injector/injector.dart';
+import 'package:crypto_app/core/theme/theme.dart';
+import 'package:crypto_app/presentation/blocs/assets/assets_bloc.dart';
 import 'package:crypto_app/presentation/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final AppConfig appConfig = AppConfig(webSocketUrl: "wss://ws.coincap.io");
+  final AppConfig appConfig = AppConfig(
+      webSocketUrl: "wss://ws.coincap.io", apiUrl: "https://api.coincap.io/v2");
 
   await setupInjector(appConfig);
   runApp(const MyApp());
@@ -16,13 +20,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final AssetsBloc assetsBloc = getIt<AssetsBloc>();
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.light().copyWith(
+          extensions: <ThemeExtension<dynamic>>[AppColorsExtension.light]),
+      home: BlocProvider(
+        create: (context) => assetsBloc..add(const GetAssets()),
+        child: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
